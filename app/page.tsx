7fb44aeae6022842580f033e3f99149cf1727cce
murilo611 <148803563+murilo611@users.noreply.github.com>
2025-04-09 21:41:06 +0100

    feat : alteração das classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,30 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/ui/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
-import { SearchIcon } from "lucide-react";
-import { Button } from "./_components/ui/button";
-import Header from "./_components/ui/header";
-import { Input } from "./_components/ui/input";
-import Image from "next/image";
-import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar } from "@radix-ui/react-avatar";
-import { AvatarImage } from "./_components/ui/avatar";
-
-
-export default function Home() {
+const Home = () => {
   return (
-    <div className=" p-5">
-
-      {/*Header} */}
-
+    <div className="p-5">
+      {/* HEADER */}
       <Header />
       <div>
-      <h2 className="text-xl fonte-blod mt-5" >Olá Murilo</h2>
-        <p>Domingo. 06 de Março de 2025</p>
+        <h2 className="mt-6 text-xl font-bold">Olá, Jimmy</h2>
+        <p>Domingo, 14 de agosto</p>
 
-        {/*Busca}*/}
-        <div className="mt-6 flex items-center gap-2"> 
-          <Input placeholder="Buscar Barbearias" />
-          <Button size={"icon"} variant="outline" >
+        {/* BUSCA */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Encontre um serviço ou salão" />
+          <Button size={"icon"}>
             <SearchIcon />
           </Button>
         </div>
 
-
-        {/*Banner} */} 
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende seus serviços nos melhores salões"
@@ -39,39 +33,37 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+        {/* AGENDAMENTO */}
+        <h2 className="mt-6 mb-3 text-xs font-bold text-gray-400 uppercase">
+          agendamentos
+        </h2>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Pentelho</h3>
 
-        {/*Agendamento} */}
-        <Card className="mt-6">
-          <CardContent className="flex justify-between ">
-           
-            {/*Esqueda} */}
-            <div className="flex flex-col gap-2 pl-0">
-            <Badge className="w-fit "> Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo </h3>
-
-              {/*Avatar */}
-             <div className="flex items-center gap-2">
+              {/* AVATAR */}
+              <div className="flex items-center gap-2">
                 <Avatar className="h-6 w-6">
                   <AvatarImage src="https://utfs.io/f/60f24f5c-9ed3-40ba-8c92-0cd1dcd043f9-16w.png"></AvatarImage>
                 </Avatar>
-                <p className="text-sm">Barbearia do Murilo</p>
-
-              
+                <p className="text-sm">Barbearia do Jimmy</p>
               </div>
             </div>
-            {/*Direita*/}
-            <div className="flex flex-col  items-center justify-center border-solid border-l-2 px-5">
-              <p className="text-sm">Fevereiro</p>
-              <p className="text-2x1 ">06</p>
-              <p className="text-sm ">09:45</p>
-      
+
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">14</p>
+              <p className="text-sm">14:30</p>
             </div>
           </CardContent>
         </Card>
-        </div>
       </div>
-  
-  );
+    </div>
+  )
 }
 
-
+export default Home
