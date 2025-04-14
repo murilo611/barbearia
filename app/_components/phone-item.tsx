f6d3ea3ff690027ba9602a6b8fdfd947078c9f2d
murilo611 <148803563+murilo611@users.noreply.github.com>
2025-04-14 21:33:49 +0100

    fix: reparing bug at copy

--- app/_components/phone-item.tsx
+++ app/_components/phone-item.tsx
@@ -1,35 +1,40 @@
 "use client"
-
-import { SmartphoneIcon } from "lucide-react";
-import { Button } from "./ui/button";
-
-
+import { SmartphoneIcon } from "lucide-react"
+import { Button } from "./ui/button"
+import { toast } from "sonner"
 
 interface PhoneItemProps {
-    phone: string;
+  phone: string
 }
 
-const PhoneItem = ({ phone }:PhoneItemProps) => {
-    
-    const handleCopyPhone = (phone:string) => {  navigator.clipboard.writeText(phone)}
-    return ( 
+const PhoneItem = ({ phone }: PhoneItemProps) => {
+  const copyPhone = () => {
+    if (navigator.clipboard?.writeText) {
+      navigator.clipboard.writeText(phone)
+    } else {
+      const el = document.createElement("textarea")
+      el.value = phone
+      document.body.appendChild(el)
+      el.select()
+      document.execCommand("copy")
+      document.body.removeChild(el)
+    }
+    toast.success("Contacto copiado com sucesso!", {
+      position: "bottom-center",
+    })
+  }
 
-         <div className="flex justify-between" key={phone}>
-            {/* Esquerda*/}
-            <div className=" flex items-center gap-2"> <SmartphoneIcon />
-              <p className="text-sm">{phone}</p>
-            </div>
-
-
-            {/* Direita*/}
-           <Button variant="outline" onClick={() => handleCopyPhone(phone)}>Copiar</Button>
-
-
-
-
-
-          </div>
-     );
+  return (
+    <div className="flex justify-between">
+      <div className="flex items-center gap-2">
+        <SmartphoneIcon />
+        <p className="text-sm">{phone}</p>
+      </div>
+      <Button variant="outline" size="sm" onClick={copyPhone}>
+        Copiar
+      </Button>
+    </div>
+  )
 }
- 
-export default PhoneItem;
+
+export default PhoneItem
