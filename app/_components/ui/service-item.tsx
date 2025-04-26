5d954716c270ca7e57119f96808a4cdf4255037f
murilo611 <148803563+murilo611@users.noreply.github.com>
2025-04-26 22:48:54 +0100

    feat:  add booking creatin logic with date hours

--- app/_components/ui/service-item.tsx
+++ app/_components/ui/service-item.tsx
@@ -1,30 +1,96 @@
 "use client"
+import { Barbershop, BarbershopService } from "@prisma/client"
+import Image from "next/image"
+import { Button } from "./button"
+import { Card, CardContent } from "./card"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from "./sheet"
+import { Calendar } from "./calendar"
+import { pt } from "date-fns/locale"
+import { useState } from "react"
+import { format, set } from "date-fns"
+import { createBooking } from "@/app/_actions/create-booking"
+import { useSession } from "next-auth/react"
+import { toast } from "sonner"
 
-import { BarbershopService } from "@prisma/client";
-import Image from "next/image";
-import { Button } from "./button";
-import { Card, CardContent } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./sheet";
-import { Calendar } from "./calendar";
-import { pt } from "date-fns/locale";
-import { useState } from "react";
-
-
-interface ServiceItemProps{
-    service: BarbershopService;
- 
+interface ServiceItemProps {
+  service: BarbershopService
+  barbershop: Pick<Barbershop, "name" | "address">
 }
 
-const ServiceItem = ({ service }: ServiceItemProps) => {
-    const [selectedDay, setSelectedDay] = useState<Date | undefined>(undefined) 
-    const handleDaySelect = (date: Date | undefined) => {
-        setSelectedDay(date)
-    }
+const TIME_LIST = [
+  "09:00",
+  "09:30",
+  "10:00",
+  "10:30",
+  "11:00",
+  "11:30",
+  "12:00",
+  "12:30",
+  "13:00",
+  "13:30",
+  "14:00",
+  "14:30",
+  "15:00",
+  "15:30",
+  "16:00",
+  "16:30",
+  "17:00",
+  "17:30",
+  "18:00",
+  "18:30",
+  "19:00",
+]
 
-    return (
-      <Card className="p-3 px-0">
-        <CardContent className="flex items-center gap-3 px-3">
-          {/* Image*/}
+const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
+  const { data } = useSession()
+  const [selectdDay, setSelectedDay] = useState<Date | undefined>(undefined)
+  const [selectedTime, setSelectedTime] = useState<string | undefined>(
+    undefined,
+  )
+  const handleDaySelect = (date: Date | undefined) => {
+    setSelectedDay(date)
+  }
+
+  const handleTimeSelect = (time: string) => {
+    setSelectedTime(time)
+  }
+
+  const handleCreateBooking = async () => {
+    
+    try {
+      if (!selectdDay || !selectedTime) return
+      const hour = Number(selectedTime.split(":")[0])
+      const minute = Number(selectedTime.split(":")[1])
+      const newDate = set(selectdDay, {
+        hours: hour,
+        minutes: minute,
+      })
+
+      await createBooking({
+        serviceId: service.id,
+        userId: "cm9n4uqgw0000wqrok3zwug31npm ",
+        date: newDate,
+      })
+      toast.success("Reserva criada com sucesso")
+    } catch (error) {
+      console.error("Erro ao criar reserva:", error)
+      toast.error("Erro ao criar reserva")
+    }
+  }
+
+  return (
+    <>
+      <Card className="p-3 px-3">
+        <CardContent className="flex items-center gap-3 px-0">
+          {/* IMAGE */}
           <div className="relative max-h-[110px] min-h-[110px] max-w-[110px] min-w-[110px]">
             <Image
               alt={service.name}
@@ -34,12 +100,12 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
             />
           </div>
 
-          {/* Direita*/}
+          {/* DIREITA */}
           <div className="space-y-2">
             <h3 className="text-sm font-bold">{service.name}</h3>
             <p className="text-sm text-gray-400">{service.description}</p>
 
-            {/* Preço e Botao */}
+            {/* PREÇO E BOTAO */}
             <div className="flex items-center justify-between">
               <p className="text-primary text-sm font-bold">
                 {Intl.NumberFormat("pt-PT", {
@@ -48,7 +114,7 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
                 }).format(Number(service.price))}
               </p>
               <Sheet>
-                <SheetTrigger>
+                <SheetTrigger asChild>
                   <Button variant="secondary" size="sm">
                     Reservar
                   </Button>
@@ -58,12 +124,12 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
                     <SheetTitle>Fazer Reserva</SheetTitle>
                   </SheetHeader>
 
-               <div className="border-b border-solid py-5">
+                  <div className="border-b border-solid py-5">
                     <Calendar
                       mode="single"
                       locale={pt}
-                      selected={selectedDay}
-                        onSelect={handleDaySelect}
+                      selected={selectdDay}
+                      onSelect={handleDaySelect}
                       styles={{
                         head_cell: {
                           width: "100%",
@@ -88,15 +154,95 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
                         },
                       }}
                     />
-                  </div>            
+                  </div>
 
+                  {/* TIME LIST */}
+                  {selectdDay && (
+                    <div className="flex-item flex gap-3 overflow-x-auto border-b border-solid p-5 pt-1 [&::-webkit-scrollbar]:hidden">
+                      {TIME_LIST.map((time) => (
+                        <Button
+                          key={time}
+                          variant={
+                            selectedTime === time ? "default" : "outline"
+                          }
+                          size="sm"
+                          className="rounded-full"
+                          onClick={() => handleTimeSelect(time)}
+                        >
+                          {time}
+                        </Button>
+                      ))}
+                    </div>
+                  )}
+
+                  {/* CARD RESERVA */}
+                  {selectedTime && selectdDay && (
+                    <div className="px-5 py-1">
+                      <Card>
+                        <CardContent className="space-y-3 p-3">
+                          <div className="flex items-center justify-between">
+                            <h2 className="font-bold">{service.name}</h2>
+                            <p className="text-sm font-bold">
+                              {Intl.NumberFormat("pt-PT", {
+                                style: "currency",
+                                currency: "EUR",
+                              }).format(Number(service.price))}
+                            </p>
+                          </div>
+
+                          <div className="flex items-center justify-between">
+                            <h2 className="text-sm text-gray-400">Data</h2>
+                            <p className="text-sm">
+                              {format(selectdDay, "d 'de' MMMM", {
+                                locale: pt,
+                              })}
+                            </p>
+                          </div>
+
+                          <div className="flex items-center justify-between">
+                            <h2 className="text-sm text-gray-400">Horário</h2>
+                            <p className="text-sm">{selectedTime}</p>
+                          </div>
+
+                          <div className="flex items-center justify-between">
+                            <h2 className="text-sm text-gray-400">Barbearia</h2>
+                            <p className="text-sm">{barbershop.name}</p>
+                          </div>
+
+                          <div className="flex items-center justify-between">
+                            <h2 className="text-sm text-gray-400">Local</h2>
+
+                            <a
+                              className="text-sm"
+                              href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                                barbershop.address,
+                              )}`}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              {barbershop.address}
+                            </a>
+                          </div>
+                        </CardContent>
+                      </Card>
+                    </div>
+                  )}
+
+                  <SheetFooter className="px-5">
+                    <SheetClose asChild>
+                      <Button size="sm" onClick={handleCreateBooking}>
+                        Confirmar Reserva
+                      </Button>
+                    </SheetClose>
+                  </SheetFooter>
                 </SheetContent>
               </Sheet>
             </div>
           </div>
         </CardContent>
       </Card>
-    )
+    </>
+  )
 }
- 
-export default ServiceItem;
+
+export default ServiceItem
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -99,7 +99,7 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       {/* Serviços*/}
       <div className="p-5">
         <h2 className="mb-3 text-xs  font-bold text-gray-400 uppercase">Serviços</h2>
-       <div className="space-y-3 text-sm text-gray-400"> {barbershop.services.map((service) =>  <ServiceItem key={service.id} service={service} />) } </div>
+       <div className="space-y-3 text-sm text-gray-400"> {barbershop.services.map((service) =>  <ServiceItem  key={service.id} barbershop={barbershop} service={service} />) } </div>
       </div>
 
       {/* Contacto*/}
